7ce86c46428475f9323ec52c9caf0e94a950bddf
GremJax <freddythorsteinson@gmail.com>
2026-03-11 23:52:01 -0800

    added reports to dashboard

--- website/js/dashboard.js
+++ website/js/dashboard.js
@@ -10,4 +10,16 @@ fetch("/me", {credentials:"include"})
     document.body.innerHTML += `<p>Hello ${data.user.username}</p>`
     document.body.innerHTML += `<a href = "https://sanndex.org/account">Account info</a>`
 
+    fetch(`/reports?userId=${data.user.id}`)
+    .then(res => res.json())
+    .then(data => {
+        
+        document.body.innerHTML += `<p>All reports:</p>`;
+
+        data.forEach(report => {
+            document.body.innerHTML += `<p>${report}</p>`;
+        })
+    
+    })
+
 })
